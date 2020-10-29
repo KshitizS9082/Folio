3e1cee41663e30f78ce8134ae4384dca54d01cc3
Kshitiz Sharma <kshitizs2809@iitkgp.ac.in>
2020-10-29 20:53:45 +0530

    made timeline but timeline and kanab board not in sync

--- Folio/Kanban/KanbanTimelineViewController.swift
+++ Folio/Kanban/KanbanTimelineViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+/*
+ warning tableviewcell maybe linked to kanbancollectionview table view cell
+ */
 class KanbanTimelineViewController: UIViewController {
     var kanban = Kanban()
     var allCards = [KanbanCard]()
@@ -80,14 +82,33 @@ class KanbanTimelineViewController: UIViewController {
    
 
 }
-extension KanbanTimelineViewController: UITableViewDataSource, UITableViewDelegate{
+extension KanbanTimelineViewController: UITableViewDataSource, UITableViewDelegate, tableCardDeletgate{
+    func updateHeights() {
+        // Disabling animations gives us our desired behaviour
+        UIView.setAnimationsEnabled(false)
+        /* These will causes table cell heights to be recaluclated,
+         without reloading the entire cell */
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        // Re-enable animations
+        UIView.setAnimationsEnabled(true)
+    }
+    
+    func updateCard(to newCard: KanbanCard) {
+        print("do nothing")
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return allCards.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell=UITableViewCell()
-        cell.backgroundColor=UIColor.blue
+        let cell = tableView.dequeueReusableCell(withIdentifier: "kabanTableCell", for: indexPath) as! CardTableViewCell
+        cell.delegate=self
+//        cell.card = board!.items[indexPath.row]
+        cell.card = self.allCards[indexPath.row]
+        cell.titleTextView.isEditable=false
+        cell.setupCard()
         return cell
     }
     
