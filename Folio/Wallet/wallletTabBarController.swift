a0e6a22a3d167d449a87cb9cd6024856fe4720d1
Kshitiz Sharma <kshitizs2809@iitkgp.ac.in>
2020-12-02 18:07:56 +0530

    wallet plus button design improvement

--- Folio/Wallet/wallletTabBarController.swift
+++ Folio/Wallet/wallletTabBarController.swift
@@ -24,19 +24,58 @@ class wallletTabBarController: UITabBarController {
     
     // TabBarButton – Setup Middle Button
     func setupMiddleButton() {
+        /*
+        var middleBtn: UIButton = {
+            let button = UIButton(frame: CGRect(x: (self.view.bounds.width / 2)-25, y: -20, width: 50, height: 50))
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitleColor(.white, for: .normal)
+            button.setTitle("More", for: .normal)
+            button.backgroundColor = .clear
+            button.titleLabel?.textAlignment = .center
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+
+            let diamond = UIView(frame: button.bounds)
+            diamond.translatesAutoresizingMaskIntoConstraints = false
+            diamond.isUserInteractionEnabled = false // button will handle touches
+            // Handle it gracefully without force unwrapping
+            button.insertSubview(diamond, belowSubview: button.titleLabel!)
+            diamond.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 4))
+            diamond.backgroundColor = .red
+            diamond.layer.cornerRadius = 10
+            diamond.widthAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+            diamond.widthAnchor.constraint(equalTo: diamond.heightAnchor).isActive = true
+            diamond.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+            diamond.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+            return button
+        }()
+        */
+        let middleBtn = UIButton(frame: CGRect(x: (self.view.bounds.width / 2)-30, y: -25, width: 60, height: 60))
+//        middleBtn.applyGradient(colors: [#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 0.3994577628).cgColor,#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 0.3918532806).cgColor])
         
-        let middleBtn = UIButton(frame: CGRect(x: (self.view.bounds.width / 2)-25, y: -20, width: 50, height: 50))
         
-        //STYLE THE BUTTON YOUR OWN WAY
-//        middleBtn.setIcon(icon: .fontAwesomeSolid(.home), iconSize: 20.0, color: UIColor.white, backgroundColor: UIColor.white, forState: .normal)
-//        middleBtn.applyGradient(colors: colorBlueDark.cgColor,colorBlueLight.cgColor])
-        middleBtn.setImage( UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 25, weight: .medium)), for: .normal)
+        middleBtn.setImage( UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 25, weight: .black)), for: .normal)
+        
         middleBtn.layer.cornerRadius = middleBtn.layer.bounds.width/2.0
-        middleBtn.backgroundColor = .systemTeal
-        middleBtn.tintColor = UIColor.systemBackground
+        middleBtn.backgroundColor = .clear
+        middleBtn.tintColor = UIColor.white
         //add to the tabbar and add click event
         self.tabBar.addSubview(middleBtn)
         middleBtn.addTarget(self, action: #selector(self.menuButtonAction), for: .touchUpInside)
+        
+        let blurView = UIImageView(frame: middleBtn.frame)
+        blurView.contentMode = .scaleToFill
+        blurView.image = UIImage(named: "financeButtonBG")!
+        blurView.layer.masksToBounds=true
+        blurView.layer.cornerRadius=blurView.frame.width/2.0
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.systemUltraThinMaterial)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = blurView.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurView.addSubview(blurEffectView)
+        blurView.sendSubviewToBack(blurEffectView)
+        self.tabBar.addSubview(blurView)
+        
+        self.tabBar.bringSubviewToFront(middleBtn)
         self.view.layoutIfNeeded()
     }
     @IBOutlet weak var rightButton: UINavigationItem!
